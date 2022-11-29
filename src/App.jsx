4915399be283c06e2570730b2c005ef92c2aa800
Luís Nunes <luis.ebn98@gmail.com>
2022-11-29 10:57:28 -0300

    initial deploy

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Routes>
+          <Route />
+        </Routes>
+      </header>
+    </div>
+  );
+}
+
+export default App;
