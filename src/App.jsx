347cc2890dc3c25b7a77100672465a5d4c387a24
Luís Nunes <luis.ebn98@gmail.com>
2022-12-02 09:47:55 -0300

    hotfix/keep login

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
+import { StyledApp } from "./AppStyle";
 import { RoutesComp } from "./routes";
 
 function App() {
   window.document.title = "KenzieHub";
   return (
-    <div className="App">
-      <header className="App-header">
-        <RoutesComp />
-      </header>
-    </div>
+    <StyledApp className="App">
+      <RoutesComp />
+    </StyledApp>
   );
 }
 
